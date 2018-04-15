0a278942a63aeaba561078af390612dce23a5fb9
jekavovikovich <jekavovikovich@gmail.com>
2018-04-15 17:24:58 +0300

    5

--- app/src/main/java/com/erappsgetsysout/MainActivity.java
+++ app/src/main/java/com/erappsgetsysout/MainActivity.java
@@ -17,12 +17,10 @@ public class MainActivity extends AppCompatActivity {
     Process process ;
     byte[] byteArry ;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         textView = (TextView)findViewById(R.id.textView);
         byteArry = new byte[1024];
