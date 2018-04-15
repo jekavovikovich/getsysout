ad21d3fe260fc4cdbfd23cf5cfa81d25e2b8dc16
jekavovikovich <jekavovikovich@gmail.com>
2018-04-15 19:53:51 +0300

    run process procedure refactored

--- app/src/main/java/com/erappsgetsysout/MainActivity.java
+++ app/src/main/java/com/erappsgetsysout/MainActivity.java
@@ -3,19 +3,14 @@ package com.erappsgetsysout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import java.io.InputStream;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView ;
-    ProcessBuilder processBuilder;
-    String Holder = "";
     String Response = "";
-    String[] DATA1 = {"/system/bin/cat", "/proc/cpuinfo"};
-    String[] DATA2 = {"/system/bin/ls", "/system/bin/"};
-    InputStream inputStream;
-    Process process ;
-    byte[] byteArry ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +18,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
-        byteArry = new byte[1024];
 
-        // first command
-        try{
-            processBuilder = new ProcessBuilder(DATA1);
-            process = processBuilder.start();
-            inputStream = process.getInputStream();
-            while(inputStream.read(byteArry) != -1){
-                Response = Holder + new String(byteArry);
+        // command
+        try {
+            Process process = Runtime.getRuntime().exec("/system/bin/cat /proc/cpuinfo");
+            InputStreamReader reader = new InputStreamReader(process.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            int numRead;
+            char[] buffer = new char[5000];
+            StringBuffer commandOutput = new StringBuffer();
+            while ((numRead = bufferedReader.read(buffer)) > 0) {
+                commandOutput.append(buffer, 0, numRead);
             }
-            inputStream.close();
-        } catch(IOException ex){
-            ex.printStackTrace();
+            bufferedReader.close();
+            process.waitFor();
+           Response = Response + "CPU Info:\n" + commandOutput.toString() + "\n";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
-        // second command
-        try{
-            processBuilder = new ProcessBuilder(DATA2);
-            process = processBuilder.start();
-            inputStream = process.getInputStream();
-            while(inputStream.read(byteArry) != -1){
-                Response = Holder + Response + Holder + new String(byteArry);
+        // command
+        try {
+            Process process = Runtime.getRuntime().exec("/system/bin/uptime");
+            InputStreamReader reader = new InputStreamReader(process.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            int numRead;
+            char[] buffer = new char[5000];
+            StringBuffer commandOutput = new StringBuffer();
+            while ((numRead = bufferedReader.read(buffer)) > 0) {
+                commandOutput.append(buffer, 0, numRead);
             }
-            inputStream.close();
-        } catch(IOException ex){
-            ex.printStackTrace();
+            bufferedReader.close();
+            process.waitFor();
+            Response = Response + "Uptime:\n" + commandOutput.toString() + "\n";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+        // command
+        try {
+            Process process = Runtime.getRuntime().exec("/system/bin/ip a");
+            InputStreamReader reader = new InputStreamReader(process.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            int numRead;
+            char[] buffer = new char[5000];
+            StringBuffer commandOutput = new StringBuffer();
+            while ((numRead = bufferedReader.read(buffer)) > 0) {
+                commandOutput.append(buffer, 0, numRead);
+            }
+            bufferedReader.close();
+            process.waitFor();
+            Response = Response + "ip a:\n" + commandOutput.toString() + "\n";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // command
+        try {
+            Process process = Runtime.getRuntime().exec("/system/bin/mount");
+            InputStreamReader reader = new InputStreamReader(process.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            int numRead;
+            char[] buffer = new char[5000];
+            StringBuffer commandOutput = new StringBuffer();
+            while ((numRead = bufferedReader.read(buffer)) > 0) {
+                commandOutput.append(buffer, 0, numRead);
+            }
+            bufferedReader.close();
+            process.waitFor();
+            Response = Response + "mount:\n" + commandOutput.toString() + "\n";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
         textView.setText(Response);
 
